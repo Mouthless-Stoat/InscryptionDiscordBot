7fd3746bb8ca33016fe8009329ee7e359e84c269
khanhfg <nphuyanh@gmail.com>
2022-03-22 07:54:54 -0300

    Added new Database system and fix old code to match with new Database system

---
+++ fightCommandDeps/database.js
@@ -0,0 +1,50 @@
+const fs = require("fs")
+
+class Database {
+    createProfile(user) {
+        fs.writeFileSync(
+            `./database/player/${user.id}.json`,
+            JSON.stringify({
+                sacMade: 0,
+                misplay: 0,
+                matchFight: 0,
+                win: 0,
+                loss: 0,
+                deckIndex: 0,
+                decks: [
+                    "",
+                    "",
+                    ""
+                ]
+            })
+        )
+    }
+
+    userExist(userID) {
+        var userExist = false
+        // find player json file in the database
+        fs.watchFile(`./database/player/${userID}.json`, () => { })
+        fs.readdirSync("./database/player").forEach(file => {
+            if (file.endsWith(".json") && file.startsWith(userID)) {
+                userExist = true
+            }
+        })
+
+        return userExist
+    }
+
+    getUserProfilePath(userID) {
+        let out = ""
+        fs.watchFile(`./database/player/${userID}.json`, () => { })
+        fs.readdirSync("./database/player").forEach(file => {
+            if (file.endsWith(".json") && file.startsWith(userID)) {
+                out = `./database/player/${userID}.json`
+            }
+        })
+        return out
+    }
+}
+
+module.exports = {
+    Database
+}
