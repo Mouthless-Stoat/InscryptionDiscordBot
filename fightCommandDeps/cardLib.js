da11a81bd9a56c51f4c9748e30cd2c88c11aa0d5
khanhfg <nphuyanh@gmail.com>
2022-03-22 16:55:55 -0300

    Transfer all Card to json file and a function to load them all when starting the bot

--- fightCommandDeps/cardLib.js
+++ fightCommandDeps/cardLib.js
@@ -1,10 +1,11 @@
 const { BroadManager } = require("./broadManager")
-const emojiUnicode = require("emoji-unicode")
+const fs = require("fs")
 
 let cardPool = []
+let sigilPool = []
 const blankID = 0
 
-class Ability {
+class Sigil {
     constructor (type = "", name = "", description = "", onActivate = (
         broadManager = new BroadManager,
         col,
@@ -17,13 +18,15 @@ class Ability {
         this.name = name
         this.description = description
         this.onActivate = onActivate
+
+        sigilPool.push(this)
     }
 }
 
-//#region defining ability
+//#region defining sigil
 
 //---- ATTACK OVERDRIVE SIGIL ----
-const flying = new Ability(
+const flying = new Sigil(
     "onAttackOverdrive",
     "Airborne",
     "This card flying over the opposing card and attack directly to the scale.",
@@ -38,7 +41,7 @@ const flying = new Ability(
 )
 
 //#region ---- ON ATTACK SIGIL ----
-const poison = new Ability(
+const poison = new Sigil(
     "onAttack",
     "Venomous",
     "This card kill the opposing card if it directly attack that card.",
@@ -50,19 +53,19 @@ const poison = new Ability(
 
 
 //#region ---- IMMUNITY SIGIL ----
-const leaping = new Ability(
+const leaping = new Sigil(
     "immunity",
     "Fly Blocker",
     "This card block incoming flying card."
 )
 
-const poisonImmunity = new Ability(
+const poisonImmunity = new Sigil(
     "immunity",
     "Poison Immunity",
     "This can can survive poisonous attack."
 )
 
-const bombImmunity = new Ability(
+const bombImmunity = new Sigil(
     "immunity",
     "Bomb Immunity",
     "This can can survive explosion."
@@ -72,7 +75,7 @@ const bombImmunity = new Ability(
 
 
 //#region ---- ON TURN END SIGIL ----
-const brittle = new Ability(
+const brittle = new Sigil(
     "onTurnEnd",
     "Brittle",
     "This card die after the owner turn end",
@@ -81,7 +84,7 @@ const brittle = new Ability(
     }
 )
 
-const photosynthesis = new Ability(
+const photosynthesis = new Sigil(
     "onTurnEnd",
     "Photosynthesis",
     "This card become stronger at the end of the owner turn",
@@ -90,7 +93,7 @@ const photosynthesis = new Ability(
     }
 )
 
-const boneDigger = new Ability(
+const boneDigger = new Sigil(
     "onTurnEnd",
     "Grave Digger",
     "This card give it owner a bone token at the end of the owner turn",
@@ -99,7 +102,7 @@ const boneDigger = new Ability(
     }
 )
 
-const healthBuff = new Ability(
+const healthBuff = new Sigil(
     "onTurnEnd",
     "Healer",
     "This card health it neighboring card at the end of the owner turn",
@@ -111,7 +114,7 @@ const healthBuff = new Ability(
     }
 )
 
-const powerBuff = new Ability(
+const powerBuff = new Sigil(
     "onTurnEnd",
     "Power Buffer",
     "This card buff it neighboring card power at the end of the owner turn",
@@ -126,7 +129,7 @@ const powerBuff = new Ability(
 
 
 //#region ---- ON TAKING DAMAGE SIGIL ----
-const spiky = new Ability(
+const spiky = new Sigil(
     "onTakingDamage",
     "Spiky",
     "This card deal 1 damage back at the opposing card after begin attack",
@@ -135,7 +138,7 @@ const spiky = new Ability(
     }
 )
 
-const panic = new Ability(
+const panic = new Sigil(
     "onTalkingDamage",
     "Panic",
     "This card attack the opposing card upon talking damage",
@@ -147,7 +150,7 @@ const panic = new Ability(
 
 
 //#region ---- ON KILL SIGIL ----
-const absorption = new Ability(
+const absorption = new Sigil(
     "onKill",
     "Absorption",
     "This card gain it opposing card stat when it kill them",
@@ -157,7 +160,7 @@ const absorption = new Ability(
     }
 )
 
-const devourer = new Ability(
+const devourer = new Sigil(
     "onKill",
     "Devourer",
     "This card regain it health when it kill another card",
@@ -170,7 +173,7 @@ const devourer = new Ability(
 
 
 //#region ---- ON PLACE SIGIL ----
-const badLuck = new Ability(
+const badLuck = new Sigil(
     "onPlace",
     "Bad luck",
     "???",
@@ -179,7 +182,7 @@ const badLuck = new Ability(
     }
 )
 
-const pride = new Ability(
+const pride = new Sigil(
     "onPlace",
     "Pride",
     "This card will have a 20% chance of calling it ally on place",
@@ -193,7 +196,7 @@ const pride = new Ability(
     }
 )
 
-const rabbitHole = new Ability(
+const rabbitHole = new Sigil(
     "onPlace",
     "Rabbit Hole",
     "This card will give it owner a rabbit on place",
@@ -202,7 +205,7 @@ const rabbitHole = new Ability(
     }
 )
 
-const markiplierFav = new Ability(
+const markiplierFav = new Sigil(
     "onPlace",
     "Markiplier's Favorite",
     'If the owner name is "Markiplier" this card get a 3/3 buff',
@@ -213,7 +216,7 @@ const markiplierFav = new Ability(
     }
 )
 
-const NollReedFav = new Ability(
+const NollReedFav = new Sigil(
     "onPlace",
     "Noll Reed Favorite",
     'If the owner name is "Noll Reed" this card gain 3 additional attack',
@@ -224,7 +227,7 @@ const NollReedFav = new Ability(
     }
 )
 
-const totemMaker = new Ability(
+const totemMaker = new Sigil(
     "onPlace",
     "Totem Maker",
     "This card will give the owner a random totem when place",
@@ -238,7 +241,7 @@ const totemMaker = new Ability(
 
 
 //#region ---- ON DEAD SIGIL ----
-const explosion = new Ability(
+const explosion = new Sigil(
     "onDead",
     "Explosion",
     "If this card die the opposing card die as well",
@@ -247,7 +250,7 @@ const explosion = new Ability(
     }
 )
 
-const scaleFate = new Ability(
+const scaleFate = new Sigil(
     "onDead",
     "Scale Fate",
     "This card fate is link to the scale, if it die the owner lost the game",
@@ -260,10 +263,20 @@ const scaleFate = new Ability(
 
 //#endregion
 
-let idCounter = 0
+function getSigilByName(name = "") {
+    let out = "error"
+    sigilPool.forEach((sigil) => {
+        if (sigil.name.toLowerCase() == name.toLocaleLowerCase()) {
+            out = sigil
+        }
+    })
+    return out
+}
+
+
 let idBank = {}
 class Card {
-    constructor (name = "", portrait = "", bloodCost = 0, boneCost = 0, power = 0, health = 1, sigilList = [], transformCard, id = -1) {
+    constructor (name = "", portrait = "", bloodCost = 0, boneCost = 0, power = 0, health = 1, sigilList = [], transformCard = null, id = -1) {
         this.name = name
         this.portrait = portrait
         this.bloodCost = bloodCost
@@ -271,7 +284,12 @@ class Card {
         this.power = power
         this.health = health
         this.sigilList = sigilList
+        this.sigilListName = []
+        sigilList.forEach(sigil => {
+            this.sigilListName.push(sigil.name)
+        })
         this.transformCard = transformCard
+
 
         if (id != -1) {
             this.id = id
@@ -407,108 +425,36 @@ function findCardInList(cardToFind = new Card, list = []) {
 }
 
 const blank = () => { return new Card("", "🔳", 0, 0, 0, 0, [], 0, 0) }
-blank()
-console.log("Loading Cards!")
 
-//#region defining cards
+fs.readdir("./database/card", (err, files) => {
+    console.log("Loading Cards!")
 
-//#region og card
-const squirrel = new Card("Squirrel", "🐿️")
-const rabbit = new Card("Rabbit", "🐰")
+    files.forEach(fileName => {
+        let temp = fs.readFileSync(`./database/card/${fileName}`, { encoding: "utf-8", flag: "r" })
+        const cardData = JSON.parse(temp)
+        let sigilList = []
+        cardData.sigilList.forEach(sigilName => {
+            sigilList.push(getSigilByName(sigilName))
+        })
 
-//---- NON SIGIL CARD ----
-const boar = new Card("Boar", "🐗", 2, 0, 3, 2)
-const tiger = new Card("Tiger", "🐯", 0, 6, 3, 3)
-const coyote = new Card("Coyote", "🦴", 0, 4, 2, 1)
-//const geck = new Card("Geck", "🦎", 0, 0, 1, 1)
-const opossum = new Card("Opossum", "🐭", 0, 2, 1, 1)
-const urayuli = new Card("Urayuli", "🦧", 4, 0, 4, 4)
-const stoat = new Card("Stoat", "🦦", 1, 0, 1, 2)
+        new Card(
+            cardData.name,
+            cardData.portrait,
+            cardData.bloodCost,
+            cardData.boneCost,
+            cardData.power,
+            cardData.health,
+            sigilList,
+            cardData.transformCard
+        )
+    })
 
-//---- SIGIL CARD ----
+    cardPool.forEach(card => {
+        console.log(`Card [${card.name}] Loaded! With ID [${card.id}]`)
+    })
 
-//---- ON ATTACK ----
-const bee = new Card("Bee", "🐝", 0, 1, 1, 1, [flying])
-const bird = new Card("Cardinal", "🐦", 2, 0, 2, 3, [flying])
-const bat = new Card("Bat", "🦇", 0, 4, 1, 2, [flying])
-const turkey = new Card("Turkey", "🦃", 0, 8, 3, 3, [flying])
-
-const scorpio = new Card("Scorpio", "🦂", 0, 4, 1, 1, [poison])
-const bug = new Card("Bug", "🐛", 1, 0, 1, 1, [poison])
-
-//---- IMMUNITY ----
-const bullfrog = new Card("Bullfrog", "🐸", 1, 0, 1, 2, [leaping])
-const iceberg = new Card("Iceberg", "🧊", 2, 0, 0, 6, [leaping])
-const tree = new Card("T H I C C  TREE", "🌳", 0, 6, 0, 6, [leaping])
-
-const badger = new Card("Honey Badger", "🦡", 1, 0, 1, 3, [poisonImmunity])
-const turtle = new Card("River Snapper", "🐢", 2, 0, 0, 6, [poisonImmunity])
-
-//---- ON TURN END ----
-const plant = new Card("Growing Seedling", "🌱", 2, 0, 1, 1, [photosynthesis])
-const sunflower = new Card("Sunflower", "🌻", 0, 6, 1, 1, [photosynthesis])
-
-const digger = new Card("Grave Digger", "🪦", 1, 0, 0, 3, [boneDigger])
-const dog = new Card("Doggo", "🐕", 2, 0, 2, 2, [boneDigger])
-
-const skeleton = new Card("Skeleton", "💀", 0, 1, 1, 1, [brittle])
-
-const HealingTotem = new Card("Healing Totem", "💞", 2, 0, 0, 3, [healthBuff])
-
-const powerTotem = new Card("Power Totem", "🦾", 0, 6, 0, 3, [powerBuff])
-
-const buffTotem = new Card("Buff Totem", "⏫", 2, 6, 0, 6, [healthBuff, powerBuff])
-
-//---- ON TALKING DAMAGE ----
-const blowfish = new Card("Blowfish", "🐡", 0, 4, 1, 2, [spiky])
-const hedgehog = new Card("Hedgehog", "🦔", 1, 0, 1, 2, [spiky])
-const cactus = new Card("Cactus", "🌵", 0, 4, 0, 4, [spiky])
-
-const scaredCat = new Card("Scared Cat", "🐈", 2, 2, 2, 2, [panic])
-
-//---- ON KILL ----
-const blankHole = new Card("Black Hole", "⚫", 2, 1, 1, 1, [absorption])
-const shark = new Card("Shark", "🦈", 0, 8, 2, 2, [absorption])
-const dragon = new Card("Dragon", "🐲", 2, 4, 2, 2, [absorption])
-
-const anaconda = new Card("Anaconda", "🐍", 2, 0, 1, 3, [devourer])
-
-//---- ON PLACE ----
-const undeadCat = new Card("Undead Cat", "🐾", 0, 0, 3, 6)
-cardPool.pop() //removing the undead cat from the card pool so the player can't get them
-const cat = new Card("Black Cat", "🐈‍⬛", 1, 0, 1, 1, [badLuck], undeadCat)
-
-const clover = new Card("Clover", "🍀", 0, 0, 2, 2, [boneDigger])
-cardPool.pop()
-const shamrock = new Card("Shamrock", "☘️", 0, 2, 1, 1, [badLuck], clover)
-
-const lion = new Card("Lion", "🦁", 3, 0, 3, 3, [pride])
-const wolf = new Card("Wolf", "🐺", 2, 0, 2, 2, [pride])
-
-const flyingUrayuli = new Card("THE FLYING URAYULI", "🪶", 4, 4, 4, 4, [flying, markiplierFav])
-
-const rat = new Card("Rat with a Gun", "🐀", 2, 0, 1, 1, [NollReedFav])
-
-const warren = new Card("Warren", "🕳️", 1, 0, 0, 3, [rabbitHole])
-
-const woodCarver = new Card("Wood Carver", "🔪", 2, 0, 1, 1, [totemMaker])
-
-//---- ON DEAD ---- 
-const bomb = new Card("Bomb", "💣", 2, 0, 0, 1, [explosion])
-
-const scale = new Card("The Scale", "⚖️", 3, 0, 5, 2, [scaleFate])
-
-
-//#endregion
-
-//#endregion
-
-
-cardPool.forEach(card => {
-    console.log(`Card [${card.name}] Loaded! With ID [${card.id}]`)
+    console.log(`${cardPool.length} Cards Loaded!`)
 })
-
-console.log(`${cardPool.length} Cards Loaded!`)
 
 module.exports = {
     Card,
