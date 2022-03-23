a54c77a5dcb362c0b874a814b7112753f49587e5
khanhfg <nphuyanh@gmail.com>
2022-03-23 17:52:01 -0300

    Update Setup Command.js to help with debugging

--- setupCommand.js
+++ setupCommand.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const { REST } = require('@discordjs/rest')
 const { Routes } = require('discord-api-types/v9')
+require("dotenv").config()
+
 //const { clientId, guildId, token } = require('./config.json')
 
 const testCommands = [
